1444e3602f4060f7f7c5f922e390d8e01e17b725
Obetta <francischassobetta@gmail.com>
2021-09-16 20:21:06 +0100

    Implement routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import styles from '../assets/css/App.module.css';
+import RestrictedRoute from './RestrictedRoute';
+import AppHeader from './AppHeader';
 import Home from './Home';
+import LoginPage from './LoginPage';
+import RegistrationPage from './RegistrationPage';
 
 function App() {
   return (
     <div className={styles.container}>
-      <Home />
+      <BrowserRouter>
+        <AppHeader />
+        <Switch>
+          <RestrictedRoute exact path="/" component={Home} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegistrationPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-import AppHeader from './AppHeader';
 import DatePanel from './DatePanel';
 import GroupPanel from '../containers/GroupPanel';
 import TaskPanel from '../containers/TaskPanel';
@@ -6,7 +5,6 @@ import TaskPanel from '../containers/TaskPanel';
 export default function Home() {
   return (
     <>
-      <AppHeader />
       <DatePanel />
       <GroupPanel />
       <TaskPanel />
